def7d782863bcdb3430b0f0058c162b71ccb4e1c
Ardoise <ardoise.gisement@gmail.com>
2013-05-16 22:51:20 +0200

    Update centralized-redis.config.sh

--- sh/centralized-redis.config.sh
+++ sh/centralized-redis.config.sh
@@ -1,7 +1,24 @@
 #!bin/bash
 
+cat <<EOF >centralized-redis.getbin.sh
+curl -O http://redis.googlecode.com/files/redis-2.6.13.tar.gz
+tar -xvfz redis-2.6.13.tar.gz
+cd redis-2.6.13
+make
+make test
+EOF
+chmod a+x centralized-redis.getbin.sh
+
+cat <<EOF >centralized-redis.cli.sh
+src/redis-cli
+echo "usage : set foo bar"
+echo "usage : get foo"
+EOF
+chmod a+x centralized-redis.cli.sh
+
 cat <<EOF >centralized-redis.sh
 src/redis-server --loglevel verbose
 EOF
+chmod a+x centralized-redis.sh
 
 exit 0;
