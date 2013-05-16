668b5e829352b58164f2bc94577dd12a42bf5397
Ardoise <ardoise.gisement@gmail.com>
2013-05-16 23:44:18 +0200

    Create distributed-shipper.config.sh

---
+++ sh/distributed-shipper.config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF >distributed-shipper.conf
+input{
+ # xyz Inputs
+ file{
+  type => "xyz-stdout-log"
+  path => [ "/.../logs/xyz_server1/xyz_server1-stdout.log" ]
+}
+output{
+  stdout { debug => true debug_format => "json"}
+  redis{
+    host => 'centralizedhost'
+    data_type => 'list'
+    key => 'logstash-redis'
+  }
+ 
+  # stdout{
+  # }
+}
+EOF
+
+cat <<EOF >distributed-shipper.sh
+nohup java -jar logstash-1.1.12-flatjar.jar agent -f distributted-shipper.conf &
+EOF
+chmod a+x distributed-shipper.sh
+
+exit 0;
