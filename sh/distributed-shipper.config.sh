ce2e533f78cf5fa82c5fcc845764c484d4f76454
Ardoise <ardoise.gisement@gmail.com>
2013-05-18 00:01:22 +0200

    Update distributed-shipper.config.sh

--- sh/distributed-shipper.config.sh
+++ sh/distributed-shipper.config.sh
@@ -8,8 +8,13 @@ cat <<EOF >distributed-shipper.conf
 input{
  # xyz Inputs
  file{
-  type => "xyz-stdout-log"
-  path => [ "/.../logs/xyz_server1/xyz_server1-stdout.log" ]
+  type =&gt; "mon_type_1"
+  path=&gt; ["/var/log/fichier1", "/var/log/fichier2"]
+ }
+ #on définit l'entrée de type fichier.
+ file {
+  type =&gt; "login" #on attribue le type login aux lignes lues dans ce fichier.
+  path =&gt; [ "/var/tmp/test"]
  }
  file {
   type => "apache-log"
@@ -18,19 +23,54 @@ input{
 }
 filter {
   grok {
+    type =&gt; "mon_type_1"
+    pattern =&gt; "%{WORD} %{WORD}"
+    # pattern =&gt; "%{WORD :premier_mot} %{WORD:second_mot}"
+  }
+  mutate {
+    type =&gt; "mon_type_1"
+    replace =&gt; ["@message","%{second_mot} %{premier_mot}"]
+  }
+  #On commence par sélectionner les lignes contenant "Login :"
+  grep {
+    match =&gt; ["@message","^Login : .*"] #ici on ne prend que les lignes commençant par Login :
+    type =&gt; "login" #le filtre s'applique aux lignes du type login.
+  }
+  #On sélectione les infos que l'on veut sur la ligne
+  grok {
+    pattern =&gt;"Login : %{WORD:login}" #ici on sélectionne le mot qui suit les deux points et le stocke dans le tableau associatif fields avec pour clé "login".
+    type =&gt; "login" #le filtre s'applique aux lignes du type login.
+  }
+  #On reforme le message avec les infos sélectionnées
+  mutate {
+    type =&gt; "login"
+    replace =&gt; ["@message","%{login}"] #On remplace le contenu du champ message en ne mettant que la valeur du login.
+  }
+  grok {
     type => "apache-log"
     pattern => "%{COMBINEDAPACHELOG}"
   }
 }
-output{
-  stdout { 
-   debug => true 
-   debug_format => "json
+output {
+  #On garde la sortie standard pour le debug, on l'enlèvera lorsque le résultat nous conviendra
+  stdout {
+    debug =&gt; true
+    debug_format => "json"
+  }
+  file {
+   path =&gt; "/var/log/mon_fichier"
+   type =&gt; "mon_type_1"
   }
   redis{
     host => 'centralized'
     data_type => 'list'
     key => 'logstash-redis'
+  }
+  file {
+    type =&gt; "login"
+    path =&gt; "/var/tmp/sortie"
+    flush_interval =&gt; 0 #on écrira dans le fichier après chaque message
+    message_format =&gt; "%{@message}" #on veut simplement écrire le contenu du champ "message"
   }
   # greylog
   gelf {
@@ -45,6 +85,16 @@ EOF
 
 cat <<EOF >distributed-shipper.sh
 nohup java -jar logstash-1.1.12-flatjar.jar agent -f distributted-shipper.conf > logger-stdout.log 2>&1&
+EOF
+chmod a+x distributed-shipper.sh
+
+cat <<EOF >distributed-shipper.test.sh
+echo test &gt; /var/tmp/test
+echo test &gt; /var/tmp/test
+echo test &gt; /var/tmp/test
+echo "Login : test" &gt; /var/tmp/test
+echo "Login : test" &gt; /var/tmp/test
+echo "Login : test" &gt; /var/tmp/test
 EOF
 chmod a+x distributed-shipper.sh
 
