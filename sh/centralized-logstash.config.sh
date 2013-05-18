1f8418c54ead546bb17093c372479dac6d37cd75
Ardoise <ardoise.gisement@gmail.com>
2013-05-19 02:46:13 +0300

    Update centralized-logstash.config.sh
    Deploy All Main centrallog

--- sh/centralized-logstash.config.sh
+++ sh/centralized-logstash.config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-./centralized-elasticsearch.config.sh
-./centralized-redis.config.sh
+./centralized-broker.config.sh
 ./centralized-indexer.config.sh
 ./centralized-shipper.config.sh
+./centralized-storagesearch.config.sh
+./centralized-webui.config.sh
+./centralized-system.config.sh
 
 exit 0;
