612d8a9864424c343bf2ff9c61de205b635b0deb
Ardoise <ardoise.gisement@gmail.com>
2013-05-19 11:14:38 +0300

    DEPLOY CENTRALIZED SERVER : BROKER, INDEXER, SHIPPER, STORAGESEARCH, WEBUI

--- sh/centralized-logstash.config.sh
+++ sh/centralized-logstash.config.sh
@@ -2,6 +2,12 @@
 
 # DEPLOY CENTRALIZED SERVER : BROKER, INDEXER, SHIPPER, STORAGESEARCH, WEBUI
 
+cat <<EOF >centralized-logstash.getbin.sh
+curl -O http://logstash.objects.dreamhost.com/release/logstash-1.1.12-flatjar.jar
+EOF
+chmod a+x centralized-logstash.getbin.sh
+
+
 ./centralized-broker.config.sh
 ./centralized-indexer.config.sh
 ./centralized-shipper.config.sh
