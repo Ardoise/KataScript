2a4d2c0ac46847c48e7920102941eaa197f47dbb
Ardoise <ardoise.gisement@gmail.com>
2013-05-19 03:10:26 +0300

    Update centralized-webui.config.sh

--- sh/centralized-webui.config.sh
+++ sh/centralized-webui.config.sh
@@ -5,11 +5,5 @@ cat <<EOF >centralized-webui.sh
 nohup java -jar logstash-1.1.12-monolithic.jar web --backend elasticsearch://127.0.0.1/ &
 EOF
 chmod a+x centralized-webui.sh;
-
-cat <<EOF >centralized-elasticsearch.cntrl.sh
-# CONTROL PORTS
-netstat -napt | grep -i LISTEN
-EOF
-chmod a+x centralized-elasticsearch.cntrl.sh
-
-exit 0
+ 
+exit 0;
