4df8e96e2b6d6ff12c7bb051894551948aae55df
Ardoise <ardoise.gisement@gmail.com>
2013-05-19 11:42:14 +0300

    Update centralized-webui.config.sh
    add test webui

--- sh/centralized-webui.config.sh
+++ sh/centralized-webui.config.sh
@@ -5,5 +5,11 @@ cat <<EOF >centralized-webui.sh
 nohup java -jar logstash-1.1.12-monolithic.jar web --backend elasticsearch://127.0.0.1/ &
 EOF
 chmod a+x centralized-webui.sh;
- 
+
+cat <<EOF >centralized-webui.test.sh
+echo -n $(date '+%d/%m/%Y %r')
+curl -s -XGET http://127.0.0.1:9292
+EOF
+chmod a+x centralized-webui.test.sh
+
 exit 0;
