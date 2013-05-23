24ee252d2d2fa46c03b1148254225fa6d7306456
Ardoise <ardoise.gisement@gmail.com>
2013-05-23 10:12:43 +0200

    Ready For Use
    - getBinaries[init] + compil[init] + start (serveur broker AMQP redis)
    - tested with redis cli

--- sh/centralized-broker.config.sh
+++ sh/centralized-broker.config.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 
-# DEPLOY CENTRALIZED SERVER : BROKER
-. ./stdlevel
-
 cat <<EOF >centralized-redis.getbin.sh
-# http://redis.io/download
-curl -O http://redis.googlecode.com/files/redis-2.6.13.tar.gz
-tar -xzf redis-2.6.13.tar.gz
-cd redis-2.6.13
-make
-make test
+#!/bin/sh
+[ -s "redis-2.6.13.tar.gz" ] || (
+  curl -OL http://redis.googlecode.com/files/redis-2.6.13.tar.gz
+  tar -xvfz redis-2.6.13.tar.gz
+)
+[ -d "redis-2.6.13" ] || (
+  cd redis-2.6.13
+  make
+  make test
+)
 EOF
 chmod a+x centralized-redis.getbin.sh
 
-cat <<EOF >centralized-redis.sh
-cd redis-2.6.13
-src/redis-server --loglevel verbose
-EOF
-chmod a+x centralized-redis.sh
-
-cat <<EOF >centralized-redis.test.sh
-cd redis-2.6.13
-cat <<ZEOF | src/redis-cli
-set foo bar
-get foo
-exit
-ZEOF
+cat <<"EOF" >centralized-redis.test.sh
+#!/bin/sh
+[ -d "redis-2.6.13" ] && (
+  cd redis-2.6.13
+  cat <<ZEOF | src/redis-cli
+  set foo bar
+  get foo
+  ZEOF
+)
 EOF
 chmod a+x centralized-redis.test.sh
 
+cat <<EOF >centralized-redis.sh
+#!/bin/sh
+[ -d "redis-2.6.13" ] && (
+  cd redis-2.6.13
+  src/redis-server --loglevel verbose
+)
+EOF
+chmod a+x centralized-redis.sh
 
 exit 0;
