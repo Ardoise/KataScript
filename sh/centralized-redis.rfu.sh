ee724ccc2799448f6e5798c6dfcc78ec3744c3ba
Ardoise <ardoise.gisement@gmail.com>
2013-05-24 18:46:07 +0200

    Update centralized-redis.rfu.sh
    ReadyForUse with Daemon

--- sh/centralized-redis.rfu.sh
+++ sh/centralized-redis.rfu.sh
@@ -2,36 +2,44 @@
 
 cat <<EOF >centralized-redis.getbin.sh
 #!/bin/sh
+
+[ -d "/opt/redis" ] || sudo mkdir -p /opt/redis;
+[ -d "/etc/redis/tmp" ] || sudo mkdir -p /etc/redis/tmp;
+
 [ -s "redis-2.6.13.tar.gz" ] || (
 curl -OL http://redis.googlecode.com/files/redis-2.6.13.tar.gz
 tar -xvfz redis-2.6.13.tar.gz
 )
 [ -d "redis-2.6.13" ] || (
-cd redis-2.6.13
-make
-make test
+  cd redis-2.6.13
+  make
+  make test
+  make install
+  sudo cp utils/redis_init_script /etc/init.d/redis
+  cp redis.conf /etc/redis/tmp/6379.conf
 )
 EOF
 chmod a+x centralized-redis.getbin.sh
 
 cat <<"EOF" >centralized-redis.test.sh
 #!/bin/sh
-[ -d "redis-2.6.13" ] && (
-  cd redis-2.6.13
-  cat <<ZEOF | src/redis-cli
+
+cat <<ZEOF | redis-cli
 set foo bar
 get foo
 ZEOF
-)
+
 EOF
 chmod a+x centralized-redis.test.sh
 
 cat <<EOF >centralized-redis.sh
 #!/bin/sh
-[ -d "redis-2.6.13" ] && (
-cd redis-2.6.13
-src/redis-server --loglevel verbose
-)
+
+# update-rc.d redis defaults
+# chkconfig redis on
+# service redis start
+/etc/init.d/redis --loglevel verbose
+
 EOF
 chmod a+x centralized-redis.sh
 
