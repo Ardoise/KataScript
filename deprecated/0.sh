72be0cbdd58c2dad57534adf7a5deb5677954d0a
Ard0ise <ardoise.gisement@gmail.com>
2013-06-10 20:53:36 +0200

    purge deprecated

--- deprecated/0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cat <<EOF >centralized-system.cntrl.sh
-# CONTROL PORTS
-netstat -napt | grep -i LISTEN
-EOF
-chmod a+x centralized-system.cntrl.sh
-
-exit 0;
--- deprecated/rest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# REST
-# Uniform Interface
-# Stateless
-# Client-Server
-# Layered System
-# Cacheable
-# Code on Demand
