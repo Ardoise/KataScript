ea8f617cf6e03eb24834c597d8faebe396971d87
Ard0ise <ardoise.gisement@gmail.com>
2013-07-13 20:26:21 +0200

    update usergroup

--- sh/abcdaire/usergroup.sh
+++ sh/abcdaire/usergroup.sh
@@ -1,5 +1,23 @@
 #!/bin/sh -e
+### BEGIN INIT INFO
+# Provides: CRUD USER GROUP 
+# Short-Description: CRUD USER GROUP
+# Author: created by: https://github.com/Ardoise
+# Update: last-update: 20130713
+### END INIT INFO
 
+# Description: SERVICE USERGROUP with suffix 'lab-'
+#
+# Requires : you need root privileges tu run this script
+#
+# CONFIG:   [ "/etc/passwd", "/etc/group" ]
+# BINARIES: [ "" ]
+# LOG:      [ "" ]
+# RUN:      [ "" ]
+# INIT:     [ "" ]
+# PLUGINS:  [ "" ]
+
+# STATELESS
 group=${group:-lab-devops};
 gid=${gid:-lab-guest};
 uid=${uid:-lab-guest};
@@ -20,14 +38,17 @@ case $group in
   *) group=lab-${group} ;;
 esac
 
+# REST
 case $1 in
 get|GET)
   [ -z "$(id -a $uid 2>/dev/null)" ] || id -a $uid;
 ;;
 put|post|PUT|POST)
   sudo groupadd -f -r $group;
-  [ -z "$(id -g $uid 2>/dev/null)" ] && sudo groupadd -r $gid;
-  [ -z "$(id -u $uid 2>/dev/null)" ] && sudo useradd --gid $gid --groups $group --password $pass $uid;
+  [ -z "$(id -g $uid 2>/dev/null)" ] && \
+  sudo groupadd -r $gid;
+  [ -z "$(id -u $uid 2>/dev/null)" ] && \
+  sudo useradd --gid $gid --groups $group --password $pass $uid;
   sudo usermod -a -G $group $uid || true;
   [ -z "$(id -a $uid 2>/dev/null)" ] || id -a $uid;
 ;;
