032a9704fb8c6a530661935b0ee63e39378df77f
Ard0ise <ardoise.gisement@gmail.com>
2013-07-14 20:31:58 +0200

    add process template

--- sh/centrallog/template-mongodb.rfu.sh
+++ sh/centrallog/template-mongodb.rfu.sh
@@ -38,20 +38,24 @@ if [ `id -u` -ne 0 ]; then
   exit $SCRIPT_ERROR
 fi
 
-# DEPENDS : OWNER
-[ -e "${SH_DIR}/lib/usergroup.sh" ] || exit 1;
-${SH_DIR}/lib/usergroup.sh POST uid=$NAME gid=$NAME group=devops pass=$NAME;
-${SH_DIR}/lib/usergroup.sh OPTION uid=$NAME;
-echo "PATH=\$PATH:/opt/$NAME" >/etc/profile.d/centrallog_$NAME.sh
+case $hh in
+install)
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : install ..."
+  
+  # DEPENDS : OWNER
+  [ -e "${SH_DIR}/lib/usergroup.sh" ] || exit 1;
+  ${SH_DIR}/lib/usergroup.sh POST uid=$NAME gid=$NAME group=devops pass=$NAME;
+  ${SH_DIR}/lib/usergroup.sh OPTION uid=$NAME;
+  echo "PATH=\$PATH:/opt/$NAME" >/etc/profile.d/centrallog_$NAME.sh
 
-mkdir -p /opt/$NAME || true; chown -R $uid:$gid /opt/$NAME || true
-mkdir -p /etc/$NAME/test || true; chown -R $uid:$gid /etc/$NAME || true
-mkdir -p /var/lib/$NAME || true; chown -R $uid:$gid /var/lib/$NAME || true
-mkdir -p /var/log/$NAME || true; chown -R $uid:$gid /var/log/$NAME || true
-mkdir -p /var/run/$NAME || true; chown -R $uid:$gid /var/run/$NAME || true
+  mkdir -p /opt/$NAME || true; chown -R $uid:$gid /opt/$NAME || true
+  mkdir -p /etc/$NAME/test || true; chown -R $uid:$gid /etc/$NAME || true
+  mkdir -p /var/lib/$NAME || true; chown -R $uid:$gid /var/lib/$NAME || true
+  mkdir -p /var/log/$NAME || true; chown -R $uid:$gid /var/log/$NAME || true
+  mkdir -p /var/run/$NAME || true; chown -R $uid:$gid /var/run/$NAME || true
 
-# Install packages necessary to compile Ruby from source
-case "$platform" in
+  # Install necessary packages
+  case "$platform" in
   Debian)
     apt-get update #--fix-missing
     apt-get -y install build-essential zlib1g-dev libssl-dev \
@@ -62,28 +66,42 @@ case "$platform" in
     apt-get -y install build-essential zlib1g-dev libssl-dev \
       libreadline-dev make curl git-core;
     ;;
-esac
-
-echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : get binaries ..."
-[ -s "template-$NAME.getbin.sh" ] && (
-  sh template-$NAME.getbin.sh;
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : get binaries [ OK ]";
-)
-echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : put config ...";
-[ -s "template-$NAME.putconf.sh" ] && (
-  sh template-$NAME.putconf.sh;
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : put config [ OK ]";
-)
-echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : start ...";
-[ -s "template-$NAME.sh" ] && (
-  sh template-$NAME.sh;
+  esac
+  
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : install [ OK ]";
+;;
+remove)
+  :
+;;
+upgrade)
+  :
+;;
+reload)
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : reload config ...";
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : reload config [ OK ]";
+;;
+start|stop|status)
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : start ...";
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : start [ OK ]";
-)
-echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : test ...";
-[ -s "template-$NAME.test.sh" ] && (
-  sh template-$NAME.test.sh;
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : test [ OK ]";
-)
+;;
+check)
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : check ...";
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: template-$NAME : check [ OK ]";
+;;
+*)
+  cat <<- _EOF_
+  Commandes :
+    check   - check centrallog::component
+    install - install centrallog::component
+    reload  - reload config centrallog::component
+    remove  - remove centrallog::component
+    start   - start centrallog::component
+    status  - status centrallog::component
+    stop    - stop centrallog::component
+    upgrade - upgrade centrallog::component
+  _EOF_
+;;
+esac
 
 unset uid gid group pass;
 
