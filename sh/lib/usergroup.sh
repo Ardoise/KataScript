49d35f9f888171dbfc4f6015c784cf20730ccd98
Ard0ise <ardoise.gisement@gmail.com>
2013-07-21 20:57:29 +0200

    update template 0.1.1 logstash

--- sh/lib/usergroup.sh
+++ sh/lib/usergroup.sh
@@ -22,7 +22,7 @@ group=${group:-lab-devops};
 gid=${gid:-lab-guest};
 uid=${uid:-lab-guest};
 pass=${pass:-lab-guest};
-uidgid="guset:guest";
+uidgid="guest:guest";
 
   : ${1?"Usage: $0 <HEAD|GET|PUT|DELETE|POST|OPTION> <uid=''> <gid=''> <group=''> <pass=''>"} # REST
   
