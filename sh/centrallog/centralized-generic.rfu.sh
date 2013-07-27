b5f8454398c4c74857f475cad9ce786f307e4b35
Ard0ise <ardoise.gisement@gmail.com>
2013-07-27 10:55:39 +0200

    update template 0.1.1 generic

--- sh/centrallog/centralized-generic.rfu.sh
+++ sh/centrallog/centralized-generic.rfu.sh
@@ -3,7 +3,7 @@
 # Provides: centrallog: xgenericx
 # Short-Description: DEPLOY SERVER: [XGENERICX]
 # Author: created by: https://github.com/Ardoise
-# Update: last-update: 20130721
+# Update: last-update: 20130727
 ### END INIT INFO
 
 # Description: SERVICE CENTRALLOG: xgenericx (...)
