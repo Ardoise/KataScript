24c864b02bc29b5524b5b7044d50992e3c2f2280
Nomade <ardoise.gisemement@gmail.com>
2013-09-02 13:30:44 +0200

    fix cloud.json

--- sh/build.0.1.1.sh
+++ sh/build.0.1.1.sh
@@ -28,29 +28,28 @@ JSON=json/cloud.json
 # max=$(cat $JSON |jq -r -c '.profil | length'; # wc -l
 for l in $(cat $JSON |jq -r -c '.profil[]'); do
 
-  i=$(echo $l | jq -r '.id'); #echo $i
-  s=$(echo $l | jq -r '.software'); #echo $s
-  e=$(echo $l | jq -r '.platform'); #echo $e
-  p=$(echo $l | jq -r '.path'); #echo $p
-  c=$(echo $l | jq -r '.conf'); #echo $c
-  g=$(echo $l | jq -r '.log'); #echo $g
-  r=$(echo $l | jq -r '.run'); #echo $r
+  i=$(echo $l | jq -r '.id'); echo -n "$i|";
+  s=$(echo $l | jq -r '.software'); echo -n "$s|";
+  e=$(echo $l | jq -r '.platform'); echo -n "$e|";
+  p=$(echo $l | jq -r '.path'); echo -n "$p|";
+  c=$(echo $l | jq -r '.conf'); echo -n "$c|";
+  g=$(echo $l | jq -r '.log'); echo -n "$g|";
+  r=$(echo $l | jq -r '.run'); echo -n "$r|";
   
-  n=$(cat $JSON | jq -r -c ".software.$s.name");
-  N=$( echo $n | tr 'a-z' 'A-Z' );
-  d=$(cat $JSON | jq -r -c ".software.$s.desc");
-  v=$(cat $JSON | jq -r -c ".software.$s.version");
-  b=$(cat $JSON | jq -r -c ".software.$s.binary");
-  u=$(cat $JSON | jq -r -c ".software.$s.download");
+  n=$(cat $JSON | jq -r -c ".software.$s.name"); echo -n "$n|";
+  N=$( echo $n | tr 'a-z' 'A-Z' ); echo -n "$N|";
+  d=$(cat $JSON | jq -r -c ".software.$s.desc"); echo -n "$d|";
+  v=$(cat $JSON | jq -r -c ".software.$s.version"); echo -n "$v|";
+  b=$(cat $JSON | jq -r -c ".software.$s.binary"); echo -n "$b|";
+  u=$(cat $JSON | jq -r -c ".software.$s.download"); echo -n "$u|";
   
-  t=$(cat $JSON | jq -r -c .process.$i.daemon.initd);
-  hi=$(cat $JSON | jq -r -c .process.$i.reload.input);  #host Input
-  ho=$(cat $JSON | jq -r -c .process.$i.reload.output); #host Output
+  t=$(cat $JSON | jq -r -c .process.$i.daemon); echo -n "$t|";
+  hi=$(cat $JSON | jq -r -c .process.$i.reload.input);  echo -n "$hi|";
+  ho=$(cat $JSON | jq -r -c .process.$i.reload.output); echo -n "$ho|";
   
-  echo "$e|$s|$v|$d|$b|$p|$t|$ho";
-  
-  [ -f "centrallog/$e-$n.tmpl.sh" ] && rm -f centrallog/$e-$n.tmpl.sh
-  # 1 pass = many changes
+  #echo "$i|$e|$s|$v|$d|$b|$p|$t|$ho";
+  echo
+  [ -f "centrallog/centralized-generic.rfu.sh" ] && (\
   sed -e "s~xgenericx~$n~g" \
       -e "s~XGENERICX~$N~g" \
       -e "s~0.0.0~$v~g" \
@@ -59,14 +58,17 @@ for l in $(cat $JSON |jq -r -c '.profil[]'); do
       -e "s~#i#download#i#~$u$b~g" \
       -e "s~#i#daemon#i#~$t~g" \
       -e "s~xlicensex~@License~g" \
-      centrallog/centralized-generic.rfu.sh > centrallog/$e-$n.tmpl.sh;
+      centrallog/centralized-generic.rfu.sh > /tmp/$e-$n.tmpl.sh;);
+      #centrallog/centralized-generic.rfu.sh > centrallog/$e-$n.tmpl.sh;);
   
+  [ -f "/tmp/$e-$n.tmpl.sh" ] && (\
   sed -i -e "/#i#start#i#/ s~.*~cat $JSON | jq -r .process.$i.start~e" \
          -e "/#i#status#i#/ s~.*~cat $JSON | jq -r .process.$i.status~e" \
          -e "/#i#stop#i#/ s~.*~cat $JSON | jq -r .process.$i.stop~e" \
          -e "/#i#pconfig#i#/ s~.*~cat $JSON | jq -r '\"PATTERN_FILE=\"+.process.$i.reload.pattern'~e" \
          -e "/#i#config#i#/ s~.*~cat $JSON | jq -r '\"CONF_FILE=\"+.process.$i.reload.conf'~e" \
-         centrallog/$e-$n.tmpl.sh;
+         /tmp/$e-$n.tmpl.sh;);
+         #centrallog/$e-$n.tmpl.sh;);
 
   #sed -i -e "/#i#update#i#/ s~.*~cat $JSON | jq -r '.dist_upgrade.install[]'~e" centrallog/centralized-$n.tmpl.sh;
 done
