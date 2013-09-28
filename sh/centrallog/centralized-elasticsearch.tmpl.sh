389cac4b9be6fdcf032ddb840b17e52055b44ac4
Ard0ise <ardoise.gisement@gmail.com>
2013-09-28 18:26:47 +0200

    fix dpkg uninstall mode sh

--- sh/centrallog/centralized-elasticsearch.tmpl.sh
+++ sh/centrallog/centralized-elasticsearch.tmpl.sh
@@ -112,7 +112,7 @@ REOF
     ;;
     *.deb)
       [ -f "$Cache$NAME/$file" ] || (cd $Cache$NAME; sudo curl -OL "$Download");
-      [ -f "$Cache$NAME/$file" ] && sudo dpkg -i $Cache$NAME/$file --root=$Bin$NAME;
+      [ -f "$Cache$NAME/$file" ] && (cd $Cache$NAME; sudo dpkg -i $file --root=$Bin$NAME);
       cat <<-REOF >$Bin$NAME/$NAME.uninstall
       pkill -u $(echo $uidgid | cut -d':' -f1);
       namepkg=$(dpkg -l |grep "$NAME" |awk -F' ' '{print $2}');
