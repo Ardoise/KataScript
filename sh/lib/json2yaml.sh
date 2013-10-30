85020eea8a53e910a1722cb3bbfeabdf9139707c
Ard0ise <ardoise.gisement@gmail.com>
2013-10-30 12:04:55 +0100

    add Ruby convert

--- sh/lib/json2yaml.sh
+++ sh/lib/json2yaml.sh
@@ -1,1 +1,7 @@
+#!/bin/bash -e
+
 ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' < file.json > file.yaml
+
+exit 0
+# alter
+python -c 'import sys, yaml, json; yaml.dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' < file.json > file.yaml
