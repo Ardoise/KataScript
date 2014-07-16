a2cc5f8cbd1d0aed2445ffcea91a79db2080293d
Ard0ise <ardoise.gisement@gmail.com>
2014-07-16 17:23:52 +0200

    KataScript 1.4.7 [ES:1.2.1, KA:3.1.0, LS:1.4.1, NG:1.7.2, contrib:Ruby:rvm:gem]

--- sh/centrallog/centralized-neo4j.tmpl.sh
+++ sh/centrallog/centralized-neo4j.tmpl.sh
@@ -166,7 +166,7 @@ install)
   #i#preinstall#i#
 
   # DOWNLOAD|CACHE + PROFIL => INSTALL => UNINSTALL
-http://dist.neo4j.org/neo4j-community-2.0.3-unix.tar.gz
+
   file=$(basename $Download);
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: test $Cache$NAME/$file";
   cd $Bin$NAME;
--- sh/centrallog/distributed-logstash.tmpl.sh
+++ sh/centrallog/distributed-logstash.tmpl.sh
@@ -166,8 +166,7 @@ install)
   #i#preinstall#i#
 
   # DOWNLOAD|CACHE + PROFIL => INSTALL => UNINSTALL
-https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.1-1-bd507eb_all.deb
-https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-bd507eb_all.deb
+
   file=$(basename $Download);
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: test $Cache$NAME/$file";
   cd $Bin$NAME;
