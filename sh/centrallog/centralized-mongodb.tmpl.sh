c57f8449f3ee3315ad7c1ec600047bb84a5e21b8
Ard0ise <ardoise.gisement@gmail.com>
2014-07-16 17:34:05 +0200

    KataScript 1.4.7 [ES:1.2.1, KA:3.1.0, LS:1.4.1, NG:1.7.2, contrib:Ruby:rvm:gem]

--- sh/centrallog/centralized-mongodb.tmpl.sh
+++ sh/centrallog/centralized-mongodb.tmpl.sh
@@ -166,7 +166,7 @@ install)
   #i#preinstall#i#
 
   # DOWNLOAD|CACHE + PROFIL => INSTALL => UNINSTALL
-
+https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.1.tgz
   file=$(basename $Download);
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: test $Cache$NAME/$file";
   cd $Bin$NAME;
