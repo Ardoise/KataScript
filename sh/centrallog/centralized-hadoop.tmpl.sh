7ee5b781e985eac33cb424f5351d8793348bc988
Ard0ise <ardoise.gisement@gmail.com>
2014-07-16 17:32:47 +0200

    KataScript 1.4.7 [ES:1.2.1, KA:3.1.0, LS:1.4.1, NG:1.7.2, contrib:Ruby:rvm:gem]

--- sh/centrallog/centralized-hadoop.tmpl.sh
+++ sh/centrallog/centralized-hadoop.tmpl.sh
@@ -166,7 +166,7 @@ install)
   #i#preinstall#i#
 
   # DOWNLOAD|CACHE + PROFIL => INSTALL => UNINSTALL
-http://apache.mirrors.lucidnetworks.net/hadoop/common/hadoop-2.4.0/hadoop-2.4.0.tar.gz
+
   file=$(basename $Download);
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: test $Cache$NAME/$file";
   cd $Bin$NAME;
